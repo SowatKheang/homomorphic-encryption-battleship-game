62cb78b9ff8c8d241e67930180964447867d66f5
Sowat <sowatkheang14@gmail.com>
2025-12-04 12:38:21 +0100

    initital file set up

---
+++ homework_3_battleship_game.py
@@ -0,0 +1,19 @@
+"""
+Applied Cryptograpy II : Homework 3 Battleship game.
+Student                : Sowat Kheang
+Howework 3             : Implementing implement a full Battleship board 10x10 matrix and each player 
+                        has 5 boats with sizes of 5, 4, 3, 2 and 2 dots using homomorphic encryption 
+                        to hide the board information
+"""
+
+import random
+from math import gcd
+
+
+# Main function
+def main():
+    print("Welcome to the Battleship Game with Homomorphic Encryption!")
+
+if __name__ == "__main__":
+    random.seed(42)
+    main()
